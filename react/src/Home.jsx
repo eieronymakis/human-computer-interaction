440e427cb7894500117ad08a0d1734e0941792aa
eieronymakis <eieronymakis@tuc.gr>
2023-07-23 15:22:23 +0300

    Fixes

--- react/src/Home.jsx
+++ react/src/Home.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import Navbar from "./Navbar";
 import './Home.css';
 import bannerRight from './image 1.png';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import Footer from "./Footer";
 import {Link} from "react-router-dom";
@@ -14,9 +12,8 @@ function Home(){
         infinite: true,
         speed: 500,
         slidesToShow: 4,
-        slidesToScroll: 2,
         autoplay: true,
-
+        autoplaySpeed: 2000
     };
 
     return (
@@ -68,51 +65,48 @@ function Home(){
                 </div>
             </div>
 
-            <div className="w-100 mt-3 d-flex flex-row-reverse align-items-center">
-                <div className="w-75 me-5">
-                    <span className="popular-title"> Δημοφιλή προϊόντα</span>
-                    <Slider {...settings} className="mt-3">
-                        <div className="popular-card bg-light">
-                            <img alt="" src="https://www.pngmart.com/files/22/iPhone-14-Pro-Max-PNG.png"></img>
-                        </div>
-                        <div className="popular-card bg-light">
-                            <img alt="" src="https://images.macrumors.com/t/Au-OUIb73hHvx2w8CirAir5bNbM=/1600x/article-new/2013/09/macbook-air-m2-roundup-header.png"></img>
-                        </div>
-                        <div className="popular-card bg-light">
-                            <img alt="" src="https://www.pngall.com/wp-content/uploads/13/Galaxy-S23-Ultra-PNG-Clipart.webp"></img>
-                        </div>
-                        <div className="popular-card bg-light">
-                            <img alt="" src="https://www.freepnglogos.com/uploads/airpods-png/airpods-pro-airpods-what-the-difference-and-10.png"></img>
-                        </div>
-                        <div className="popular-card bg-light">
-                            <img alt="" src="https://dlcdnwebimgs.asus.com/gain/E0E15911-885A-421B-A775-5E1854731699"></img>
-                        </div>
-                    </Slider>
-                </div>
+            <div className="w-75 ms-auto me-5 mt-5">
+            <span className="popular-title"> Δημοφιλή προϊόντα</span>
+                <Slider {...settings} className="mt-3">
+                    <div className="popular-card bg-light">
+                        <img alt="" src="https://www.pngmart.com/files/22/iPhone-14-Pro-Max-PNG.png"></img>
+                    </div>
+                    <div className="popular-card bg-light">
+                        <img alt="" src="https://images.macrumors.com/t/Au-OUIb73hHvx2w8CirAir5bNbM=/1600x/article-new/2013/09/macbook-air-m2-roundup-header.png"></img>
+                    </div>
+                    <div className="popular-card bg-light">
+                        <img alt="" src="https://www.pngall.com/wp-content/uploads/13/Galaxy-S23-Ultra-PNG-Clipart.webp"></img>
+                    </div>
+                    <div className="popular-card bg-light">
+                        <img alt="" src="https://www.freepnglogos.com/uploads/airpods-png/airpods-pro-airpods-what-the-difference-and-10.png"></img>
+                    </div>
+                    <div className="popular-card bg-light">
+                        <img alt="" src="https://dlcdnwebimgs.asus.com/gain/E0E15911-885A-421B-A775-5E1854731699"></img>
+                    </div>
+                </Slider>
             </div>
 
-            <div className="w-100 mt-3 d-flex flex-row-reverse align-items-center">
-                <div className="w-75 me-5">
-                    <span className="popular-title"> Νέα προϊόντα</span>
-                    <Slider {...settings} className="mt-3">
-                        <div className="popular-card bg-light">
-                            <img alt="" src="https://www.in-win.com/uploads/Product/gaming-chassis/d-frame-2/d-frame2_overview_options_02.png"></img>
-                        </div>
-                        <div className="popular-card bg-light">
-                            <img alt="" src="https://asset.msi.com/resize/image/global/product/product_1665552671c76199be0956de9b63d7e35b33b91acb.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png"></img>
-                        </div>
-                        <div className="popular-card bg-light">
-                            <img alt="" src="https://shop.asisnet.gr/wp-content/uploads/2022/06/DELL-Server-PowerEdge-R550-2U.png"></img>
-                        </div>
-                        <div className="popular-card bg-light">
-                            <img alt="" src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Alienware_x14_3.png"></img>
-                        </div>
-                        <div className="popular-card bg-light">
-                            <img alt="" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f0e13eb8-4db6-4a3e-97ea-918a4dce742b/deb2976-e88ef583-9b52-4035-a879-b0cdfb76c084.png/v1/fill/w_723,h_720/ps5_disc_edition_box__united_states___png__by_regularshowfan2005_deb2976-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvZjBlMTNlYjgtNGRiNi00YTNlLTk3ZWEtOTE4YTRkY2U3NDJiXC9kZWIyOTc2LWU4OGVmNTgzLTliNTItNDAzNS1hODc5LWIwY2RmYjc2YzA4NC5wbmciLCJ3aWR0aCI6Ijw9NzIzIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.3gTn415DLTV7u9SGvVsTSENm66639jafTSDK2EALePA"></img>
-                        </div>
-                    </Slider>
-                </div>
+            <div className="w-75 ms-auto me-5 mt-5">
+                <span className="popular-title"> Νέα προϊόντα</span>
+                <Slider {...settings} className="mt-3">
+                    <div className="popular-card bg-light">
+                        <img alt="" src="https://www.in-win.com/uploads/Product/gaming-chassis/d-frame-2/d-frame2_overview_options_02.png"></img>
+                    </div>
+                    <div className="popular-card bg-light">
+                        <img alt="" src="https://asset.msi.com/resize/image/global/product/product_1665552671c76199be0956de9b63d7e35b33b91acb.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png"></img>
+                    </div>
+                    <div className="popular-card bg-light">
+                        <img alt="" src="https://shop.asisnet.gr/wp-content/uploads/2022/06/DELL-Server-PowerEdge-R550-2U.png"></img>
+                    </div>
+                    <div className="popular-card bg-light">
+                        <img alt="" src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Alienware_x14_3.png"></img>
+                    </div>
+                    <div className="popular-card bg-light">
+                        <img alt="" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f0e13eb8-4db6-4a3e-97ea-918a4dce742b/deb2976-e88ef583-9b52-4035-a879-b0cdfb76c084.png/v1/fill/w_723,h_720/ps5_disc_edition_box__united_states___png__by_regularshowfan2005_deb2976-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvZjBlMTNlYjgtNGRiNi00YTNlLTk3ZWEtOTE4YTRkY2U3NDJiXC9kZWIyOTc2LWU4OGVmNTgzLTliNTItNDAzNS1hODc5LWIwY2RmYjc2YzA4NC5wbmciLCJ3aWR0aCI6Ijw9NzIzIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.3gTn415DLTV7u9SGvVsTSENm66639jafTSDK2EALePA"></img>
+                    </div>
+                </Slider>
             </div>
+
 
             <Footer></Footer>
         </div>
